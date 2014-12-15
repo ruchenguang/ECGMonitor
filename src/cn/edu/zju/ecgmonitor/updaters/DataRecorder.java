f9b62b3f16cdbc1366953e51e9afb25599070a0c
Chenguang Ru <ruchenguang@gmail.com>
2014-12-15 13:41:11 +0800

    ECG Monitor
    This is a monitor app for Nordic board developed by VLSI5@ZJU

---
+++ src/cn/edu/zju/ecgmonitor/updaters/DataRecorder.java
@@ -0,0 +1,50 @@
+package cn.edu.zju.ecgmonitor.updaters;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class DataRecorder {
+	File recordFile;
+	FileOutputStream fos;
+	String fileName, recordDirPath;
+	public DataRecorder(String recordDirPath){
+		this.recordDirPath = recordDirPath;
+	}
+	
+	public void startWriting(){
+		Date currentDate = new Date(System.currentTimeMillis()); 
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.CHINA); 
+		fileName = formatter.format(currentDate) + ".txt";
+		
+		recordFile = new File(recordDirPath, fileName);
+		try {
+			fos = new FileOutputStream(recordFile);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void writeToFile(byte[] bleRawData){
+		try {
+			fos.write(bleRawData);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+	
+	public void stopWriting(){
+		try {
+			if(fos != null) fos.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
